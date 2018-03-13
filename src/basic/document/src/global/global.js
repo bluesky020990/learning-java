358e2271f481a4cd3cae244c02057497d794b26d
KhanhTran <bluesky020990@gmail.com>
2018-03-13 23:34:39 +0700

    Type of Polymorphism in Java.

--- src/basic/document/src/global/global.js
+++ src/basic/document/src/global/global.js
@@ -1,9 +1,23 @@
+const LIST_LESSON_DATA = [
+    "../javaartecture/lesson_00_Beginning_Learning_java.html",
+    "../javaartecture/lesson_01_b_Java_Vitual_Machine.html",
+    "../javaartecture/lesson_02_c_Components_of_JDK.html",
+    "../javaartecture/lesson_03_d_Obbject_Reference.html",
+
+    "../javabasic/lesson_05_Object_n_Class_continous.html",
+    "../javabasic/lesson_06_Object_n_Class_end.html",
+    "../javabasic/lesson_07_Access_Modifier_in_Java.html",
+    "../javabasic/lesson_08_Constructor_in_Java.html",
+    "../javabasic/lesson_09_Keywords_in_Class.html",
+    "../javabasic/lesson_10_How_System_out_in_Java_works.html",
 
 
+    "../javaoop/lesson_11_Inheritance_in_Java.html",
+    "../javaoop/lesson_12_Polymorphism_in_Java.html",
+    "../javaoop/lesson_13_Type_of_Polymorphism_in_Java.html",
+    "../javaoop/lesson_14_Abstract_in_Java.html",
 
-
-
-
+];
 
 
 $(document).ready(function(){
